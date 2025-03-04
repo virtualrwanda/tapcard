2c5b3e9ea4c3b5e24b08144177fb273927c92406
virtualrwanda <virtualrwandatechnology@gmail.com>
2025-03-04 14:07:19 +0200

    Update app.py
    False Debugging

--- app.py
+++ app.py
@@ -1287,4 +1287,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Create database tables
     threading.Thread(target=start_all_periodic_reports).start()
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
