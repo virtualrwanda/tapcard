ac59c125459261e2769356ad9d156ee4326d7c2d
virtualrwanda <virtualrwandatechnology@gmail.com>
2025-05-14 16:50:28 +0200

    Update app.py
    anovelcard@masagital.com update

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'
 app.config['MAIL_SERVER'] = 'mail.vrt.rw'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'info@vrt.rw'
+app.config['MAIL_USERNAME'] = 'anovelcard@masagital.com'
 app.config['MAIL_PASSWORD'] = 'TheGreat@123'
 mail = Mail(app)
 
